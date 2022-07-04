75d91a447c650847bb93cdd87c7114ff15e32cd6
DennyAzevedo <denny_af@yahoo.com.br>
2022-07-04 19:27:01 -0300

    Criando Rotas e Controllers

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express'
+
+export const home = (req: Request, res: Response) => {
+	res.send('home no controller')
+	//res.render('page/page')
+}
+
+export const dogs = (req: Request, res: Response) => {
+	//res.render('page/page')
+}
+
+export const cats = (req: Request, res: Response) => {
+	//res.render('page/page')
+}
+
+export const fishes = (req: Request, res: Response) => {
+	//res.render('page/page')
+}
