8a13b1a23fe815c5896d526d044d319a36c481c8
DennyAzevedo <denny_af@yahoo.com.br>
2022-07-04 19:44:35 -0300

    Layout Inicial - Views

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,18 +1,17 @@
 import { Request, Response } from 'express'
 
 export const home = (req: Request, res: Response) => {
-	res.send('home no controller')
-	//res.render('page/page')
+	res.render('pages/page')
 }
 
 export const dogs = (req: Request, res: Response) => {
-	//res.render('page/page')
+	//res.render('pages/page')
 }
 
 export const cats = (req: Request, res: Response) => {
-	//res.render('page/page')
+	//res.render('pages/page')
 }
 
 export const fishes = (req: Request, res: Response) => {
-	//res.render('page/page')
+	//res.render('pages/page')
 }
